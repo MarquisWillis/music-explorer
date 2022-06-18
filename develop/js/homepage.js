4bac80e85540945818083fd033b5d8134352c79f
Marquis Willis <marquiswillis99@gmail.com>
2022-06-18 13:52:48 -0400

    added foundation for selectors and functions is both JS files

--- develop/js/homepage.js
+++ develop/js/homepage.js
@@ -1,1 +1,21 @@
 // ADD PSUEDO CODE
+
+// static homepage selectors
+
+// form element selectors
+let searchForm = document.querySelector("#search-form");
+let artistInput = document.querySelector("#artist-input");
+let genreInput = document.querySelector("#genre-input");
+let locationInput = document.querySelector("#location-input");
+
+let genreContainerEl = document.querySelector("#genre-container"); // TBD on whether or not to use this element or individuals
+
+// let eventLocationURL = `https://serpapi.com/search.json?engine=google_events&q=Events+in+${locationInput.value}+${artistInput.value}`;
+
+// let eventGenreURL = `https://serpapi.com/search.json?engine=google_events&q=Events+in+${locationInput.value}+${artistInput.value}`
+
+function generateResults(event) {
+    event.preventDefault();
+}
+
+searchForm.addEventListener("submit", generateResults);
