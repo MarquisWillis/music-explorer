05ea781ca0208a5a8245b47ef1f9ca052abe9b56
Marquis Willis <marquiswillis99@gmail.com>
2022-06-19 17:23:19 -0400

    updated homepageJS funcitons to store query data into local storage, formatted html documents

--- develop/js/homepage.js
+++ develop/js/homepage.js
@@ -8,14 +8,45 @@ let artistInput = document.querySelector("#artist-input");
 let genreInput = document.querySelector("#genre-input");
 let locationInput = document.querySelector("#location-input");
 
-let genreContainerEl = document.querySelector("#genre-container"); // TBD on whether or not to use this element or individuals
+// genre card selectors
+let genreContainerEl = document.querySelectorAll(".genre-card"); 
 
-// let eventLocationURL = `https://serpapi.com/search.json?engine=google_events&q=Events+in+${locationInput.value}+${artistInput.value}`;
+// form function
+/*
+    - retrieve values from inputs in the form element
+    - save values in local storage
 
-// let eventGenreURL = `https://serpapi.com/search.json?engine=google_events&q=Events+in+${locationInput.value}+${artistInput.value}`
-
+*/
 function generateResults(event) {
+
     event.preventDefault();
+
+    let artist = artistInput.value;
+    let genre = genreInput.value;
+    let location = locationInput.value;
+
+    localStorage.setItem("artist", artist);
+    localStorage.setItem("genre", genre);
+    localStorage.setItem("location", location);
+
+    displaySearch();
 }
 
+// genre card function (similar to generateResults)
+ function genreSearch(event) {
+
+    let genre = event.target.textContent.trim();
+
+    localStorage.setItem("genre", genre);
+
+    displaySearch();
+ }
+
+ // redirects homepage to search page
+ function displaySearch() {
+    location.replace("./search-page.html");
+ }
+
+ // add event handlers for search form and genre cards respectively
 searchForm.addEventListener("submit", generateResults);
+genreContainerEl.addEventListener("click", genreSearch);
