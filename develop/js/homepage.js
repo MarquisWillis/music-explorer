11dce805dd014e1abb62d9c700eee6ae8ee68375
Marquis Willis <marquiswillis99@gmail.com>
2022-06-24 23:00:23 -0400

    successfully retrieved ticketmaster api info

--- develop/js/homepage.js
+++ develop/js/homepage.js
@@ -4,12 +4,10 @@
 let artistInput = document.querySelector("#artist-input");
 let genreInput = document.querySelector("#genre-input");
 let locationInput = document.querySelector("#location-input");
-let searchBtn = document.querySelector("#search-btn");
-
-let genreCardEl = document.querySelectorAll(".genreCard");
+let searchBtn = document.querySelector("#searchButton");
 
 let searchContainerEl = document.querySelector("#search-result");
-let additionalContainerEl = document.querySelector("#additional-container");
+let eventContainerEl = document.querySelector("#event-container");
 
 const token = "a8yMuGaFL4Zrzhb2V2sOhHkAOPVwaAEy";
 
@@ -17,16 +15,43 @@ const token = "a8yMuGaFL4Zrzhb2V2sOhHkAOPVwaAEy";
 // functions
 
 function testTicket() {
-    let artist = (artistInput.value);
-    let location = (locationInput.value);
+    let artist = encodeURI(artistInput.value);
+    let location = encodeURI(locationInput.value);
 
-    let urlCurrent = `https://app.ticketmaster.com/discovery/v2/events?apikey=${token}&keyword=${artist}&locale=*&city=${location}&callback=eventHandler`
+    let urlCurrent = `https://app.ticketmaster.com/discovery/v2/events?apikey=${token}&keyword=${artist}&locale=*&city=${location}`
 
     fetch(urlCurrent).then(function(response) {
         console.log(response);
         return response.json();
+    }).then(function(data) {
+        console.log(data);
+
+        for (let i = 0; i < data._embedded.events.length; i++) {
+
+            
+            let currentDate = data._embedded.events[i].dates.start.localDate;
+            let currentTime = data._embedded.events[i].dates.start.localTime;
+
+            console.log(currentDate, currentTime)
+
+
+            let eventCard = document.createElement("div")
+
+            let dateEl = document.createElement("p")
+            let timeEl = document.createElement("p");
+            
+            dateEl.textContext = currentDate
+            timeEl.textContext = currentTime
+            
+            eventCard.appendChild(dateEl)
+            eventCard.appendChild(timeEl)
+            
+            eventContainerEl.appendChild(eventCard)
+        }
     })
-}
+
+        
+    }
 
 // event listeners
 searchBtn.addEventListener("click", testTicket);
